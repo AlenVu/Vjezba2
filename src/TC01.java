37ca258f5fc9327b8f34588832ebaaf52734aa63
AlenVu <alen.jakumetovic@gmail.com>
2022-12-14 13:25:09 +0100

    add new additional line to TC01

--- src/TC01.java
+++ src/TC01.java
@@ -3,6 +3,7 @@ public class TC01 {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("Hello Balkan");
+        System.out.println("Hello Europe");
     }
 
 }
